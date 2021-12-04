b2c6f5954b98b9aaf0e793e3471adb02a19ea64c
Jess <jess@ros.io>
2021-12-04 13:23:10 -0700

    Update simple_worker example to include signal monitoring and shutdown

--- fang_examples/simple_worker/src/main.rs
+++ fang_examples/simple_worker/src/main.rs
@@ -4,6 +4,7 @@ use fang::RetentionMode;
 use fang::WorkerParams;
 use fang::WorkerPool;
 use simple_worker::MyJob;
+use signal_hook::{consts::signal::*, iterator::Signals};
 
 fn main() {
     dotenv().ok();
@@ -13,12 +14,28 @@ fn main() {
     let mut worker_params = WorkerParams::new();
     worker_params.set_retention_mode(RetentionMode::KeepAll);
 
-    WorkerPool::new_with_params(2, worker_params).start();
+    let mut worker_pool = WorkerPool::new_with_params(2, worker_params);
+    worker_pool.start().unwrap();
 
     let queue = Queue::new();
 
     queue.push_task(&MyJob::new(1)).unwrap();
     queue.push_task(&MyJob::new(1000)).unwrap();
 
-    std::thread::park();
+    let mut signals = Signals::new(&[SIGINT, SIGTERM]).unwrap();
+    for sig in signals.forever() {
+        match sig {
+            SIGINT => {
+                println!("Received SIGINT");
+                worker_pool.shutdown().unwrap();
+                break;
+            },
+            SIGTERM => {
+                println!("Received SIGTERM");
+                worker_pool.shutdown().unwrap();
+                break;
+            },
+            _ => unreachable!(format!("Received unexpected signal: {:?}", sig)),
+        }
+    }
 }
