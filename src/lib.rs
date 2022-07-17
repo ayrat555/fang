b1fbe8c5afe7002f25e4d64109cae1ddbd20ede6
Ayrat Badykov <ayratin555@gmail.com>
2022-07-17 09:10:32 +0300

    create sync mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
-#![allow(clippy::nonstandard_macro_braces)]
-#![allow(clippy::extra_unused_lifetimes)]
+// #![allow(clippy::nonstandard_macro_braces)]
+// #![allow(clippy::extra_unused_lifetimes)]
+
+// pub mod error;
+// pub mod executor;
+// pub mod queue;
+// pub mod scheduler;
+// pub mod schema;
+// pub mod worker_pool;
+
+// pub use error::FangError;
+// pub use executor::*;
+// pub use queue::*;
+// pub use scheduler::*;
+// pub use schema::*;
+// pub use worker_pool::*;
 
 #[macro_use]
 extern crate diesel;
-
-pub mod error;
-pub mod executor;
-pub mod queue;
-pub mod scheduler;
-pub mod schema;
-pub mod worker_pool;
-
-pub use error::FangError;
-pub use executor::*;
-pub use queue::*;
-pub use scheduler::*;
-pub use schema::*;
-pub use worker_pool::*;
 
 #[doc(hidden)]
 pub use diesel::pg::PgConnection;
 #[doc(hidden)]
 pub use typetag;
+
+pub mod sync;
+pub use sync::*;
