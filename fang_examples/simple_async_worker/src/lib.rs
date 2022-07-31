079f391c1ea075442659b05e026727e78ff0dfeb
Ayrat Badykov <ayratin555@gmail.com>
2022-07-31 15:06:32 +0300

    fix

--- fang_examples/simple_async_worker/src/lib.rs
+++ fang_examples/simple_async_worker/src/lib.rs
@@ -18,15 +18,15 @@ impl MyTask {
     }
 }
 
-#[typetag::serde]
 #[async_trait]
+#[typetag::serde]
 impl AsyncRunnable for MyTask {
     async fn run(&self, queue: &mut dyn AsyncQueueable) -> Result<(), Error> {
         log::info!("the curreny number is {}", self.number);
         tokio::time::sleep(Duration::from_secs(3)).await;
 
         let new_task = MyTask::new(self.number + 1);
-        let metadata = serde_json::to_value(new_task).unwrap();
+        let metadata = serde_json::to_value(&new_task as &dyn AsyncRunnable).unwrap();
         queue.insert_task(metadata, "common").await.unwrap();
 
         Ok(())
