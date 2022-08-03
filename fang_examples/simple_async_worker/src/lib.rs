cf0dde90136a251bf7ba6659ed2c0d0a33c395a4
Pmarquez <48651252+pxp9@users.noreply.github.com>
2022-08-03 08:27:38 +0000

    Update fang_examples/simple_async_worker/src/lib.rs
    Co-authored-by: Ayrat Badykov <ayratin555@gmail.com>

--- fang_examples/simple_async_worker/src/lib.rs
+++ fang_examples/simple_async_worker/src/lib.rs
@@ -22,7 +22,7 @@ impl MyTask {
 #[typetag::serde]
 impl AsyncRunnable for MyTask {
     async fn run(&self, queue: &mut dyn AsyncQueueable) -> Result<(), Error> {
-        log::info!("the currently number is {}", self.number);
+        log::info!("the current number is {}", self.number);
         tokio::time::sleep(Duration::from_secs(3)).await;
 
         let new_task = MyTask::new(self.number + 1);
