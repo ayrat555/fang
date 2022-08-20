c24b92e349f617f29ace07df2f27cb2f095273c0
Ayrat Badykov <ayratin555@gmail.com>
2022-08-20 18:52:33 +0300

    change schema

--- migrations/2021-07-24-050243_create_fang_periodic_tasks/down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE fang_periodic_tasks;
--- migrations/2022-08-20-151615_create_fang_tasks/up.sql
+++ migrations/2022-08-20-151615_create_fang_tasks/up.sql
@@ -6,13 +6,16 @@ CREATE TABLE fang_tasks (
      id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
      metadata jsonb NOT NULL,
      error_message TEXT,
-     state fang_task_state default 'new' NOT NULL,
-     task_type VARCHAR default 'common' NOT NULL,
+     state fang_task_state DEFAULT 'new' NOT NULL,
+     task_type VARCHAR DEFAULT 'common' NOT NULL,
+     periodic BOOLEAN DEFAULT FALSE,
+     uniq_hash CHAR(64),
+     scheduled_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
      created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
      updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX fang_tasks_state_index ON fang_tasks(state);
 CREATE INDEX fang_tasks_type_index ON fang_tasks(task_type);
-CREATE INDEX fang_tasks_created_at_index ON fang_tasks(created_at);
-CREATE INDEX fang_tasks_metadata_index ON fang_tasks(metadata);
+CREATE INDEX fang_tasks_scheduled_at_index ON fang_tasks(scheduled_at);
+CREATE INDEX fang_tasks_uniq_hash ON fang_tasks(uniq_hash);
