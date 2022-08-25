aa25f3f8f1a99c1613725b03953b7d985c260eea
pxp9 <48651252+pxp9@users.noreply.github.com>
2022-08-25 23:28:42 +0200

    Current timestamp value

--- src/asynk/queries/fetch_task_type.sql
+++ src/asynk/queries/fetch_task_type.sql
@@ -1,1 +1,1 @@
-SELECT * FROM fang_tasks  WHERE task_type = $1 AND state = 'new' AND NOW() <= scheduled_at  ORDER BY created_at ASC, scheduled_at ASC LIMIT 1 FOR UPDATE SKIP LOCKED
+SELECT * FROM fang_tasks  WHERE task_type = $1 AND state = 'new' AND CURRENT_TIMESTAMP <= scheduled_at  ORDER BY created_at ASC, scheduled_at ASC LIMIT 1 FOR UPDATE SKIP LOCKED
