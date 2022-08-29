91f2fdf330b7c1cb6b97b77bf98a1f2218832f71
Pmarquez <48651252+pxp9@users.noreply.github.com>
2022-08-29 22:28:43 +0200

    Update asynk.rs

--- src/asynk.rs
+++ src/asynk.rs
@@ -2,6 +2,7 @@ pub mod async_queue;
 pub mod async_runnable;
 pub mod async_worker;
 pub mod async_worker_pool;
+
 pub use async_queue::*;
 pub use async_runnable::AsyncRunnable;
 pub use async_runnable::Error as AsyncError;
