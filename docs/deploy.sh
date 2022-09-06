10c3150fff85e6176ec7a2d258448f93b2665af2
pxp9 <pepe.marquezromero@gmail.com>
2022-09-06 20:38:40 +0200

    CHANGELOG AND README update automated

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -3,9 +3,33 @@ set -e
 
 BRANCH="gh-pages"
 
+current_time() {
+  
+  TIME=$(date -u --rfc-3339=seconds)
+
+  DATE=$(echo $TIME | cut -d' ' -f1)
+  HOUR=$(echo $TIME | cut -d' ' -f2)
+  
+  VALID_TIME=${DATE}T${HOUR}
+  
+  echo $VALID_TIME
+
+}
+
 build() {
   echo "Starting building..."
+  
+  TIME=$(current_time)
 
+  printf "+++\ntitle = \"CHANGELOG\"\ndescription = \"Fang CHANGELOG\"\ndate = $TIME\nupdated = $TIME\ndraft = false\nweight = 410\nsort_by = \"weight\"\ntemplate = \"docs/page.html\"\n\n[extra]\nlead = \"Fang Changelog\"\ntoc = true\ntop = false\n+++\n\n" > docs/content/docs/CHANGELOG.md
+
+  cat CHANGELOG.md >> docs/content/docs/CHANGELOG.md
+
+  printf "+++\ntitle = \"README\"\ndescription = \"Fang README\"\ndate = $TIME\nupdated = $TIME\ndraft = false\nweight = 410\nsort_by = \"weight\"\ntemplate = \"docs/page.html\"\n\n[extra]\nlead = \"Fang README\"\ntoc = true\ntop = false\n+++\n\n" > docs/content/docs/README.md
+
+  cat README.md >> docs/content/docs/README.md
+
+  
   cp -R docs ../docs_backup
   rm -r *
   cp -R ../docs_backup ./docs
