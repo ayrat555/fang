4b49947fe3994749257dc1d681f68825e47abb8d
pxp9 <pepe.marquezromero@gmail.com>
2022-10-17 09:37:37 +0200

    Documenting Async runnable

--- src/asynk/async_runnable.rs
+++ src/asynk/async_runnable.rs
@@ -37,27 +37,51 @@ impl From<SerdeError> for FangError {
     }
 }
 
+/// Implement this trait to insert your own type of task.
 #[typetag::serde(tag = "type")]
 #[async_trait]
 pub trait AsyncRunnable: Send + Sync {
+    /// This will define what the task do.
     async fn run(&self, client: &mut dyn AsyncQueueable) -> Result<(), FangError>;
 
+    /// This will define what is the task type.
+    /// By default `COMMON_TYPE` constant is the task type
     fn task_type(&self) -> String {
         COMMON_TYPE.to_string()
     }
 
+    /// If set to true, library will handle the uniqueness of the task that implements this trait.
     fn uniq(&self) -> bool {
         false
     }
 
+    /// This will define if a task is periodic or is scheduled once in the time.
+    /// You can see [how cron format works](https://docs.oracle.com/cd/E12058_01/doc/doc.1014/e12030/cron_expressions.htm)
+    /// Be careful works only with UTC timezone.
+    /// [Web to check timezones](https://www.timeanddate.com/worldclock/timezone/utc)
+    ///
+    /// Example:
+    ///
+    //!```rust
+    //! fn cron(&self) -> Option<Scheduled> {
+    //!     let expression = "0/20 * * * Aug-Sep * 2022/1";
+    //!     Some(Scheduled::CronPattern(expression.to_string()))
+    //! }
+    //!```
+    /// In order to schedule once a task use, `Scheduled::ScheduleOnce` variant.
+    /// And make a Date. 
     fn cron(&self) -> Option<Scheduled> {
         None
     }
 
+    /// This will define the number of retries that will do if a task fails.
+    /// By default the number of retries will be `RETRIES_NUMBER` constant, which value is 20.
     fn max_retries(&self) -> i32 {
         RETRIES_NUMBER
     }
-
+    
+    /// This will define the number of seconds that will increment after an attempt of a fail task.
+    /// By default is exponential,  2^(attempt)
     fn backoff(&self, attempt: u32) -> u32 {
         u32::pow(2, attempt)
     }
