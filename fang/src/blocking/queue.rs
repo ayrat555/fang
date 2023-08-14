d641e57416d6f7c79b385966cf804668956d121f
Dopplerian <dopplerian+git@proton.me>
2023-08-14 21:36:25 +0200

    make `blocking::queue` tests use only `Queueable` methods when interacting with `Queue`

--- fang/src/blocking/queue.rs
+++ fang/src/blocking/queue.rs
@@ -510,6 +510,7 @@ mod queue_tests {
     use diesel::connection::Connection;
     use diesel::result::Error;
     use serde::{Deserialize, Serialize};
+    use serial_test::serial;
 
     #[derive(Serialize, Deserialize)]
     struct PepeTask {
@@ -577,6 +578,7 @@ mod queue_tests {
     }
 
     #[test]
+    #[serial]
     fn insert_task_test() {
         let task = PepeTask { number: 10 };
 
@@ -584,25 +586,22 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task = Queue::insert_query(conn, &task, Utc::now()).unwrap();
+        let task = queue.insert_task(&task).unwrap();
 
-            let metadata = task.metadata.as_object().unwrap();
-            let number = metadata["number"].as_u64();
-            let type_task = metadata["type"].as_str();
+        let metadata = task.metadata.as_object().unwrap();
+        let number = metadata["number"].as_u64();
+        let type_task = metadata["type"].as_str();
 
-            assert_eq!(task.error_message, None);
-            assert_eq!(FangTaskState::New, task.state);
-            assert_eq!(Some(10), number);
-            assert_eq!(Some("PepeTask"), type_task);
-
-            Ok(())
-        });
+        assert_eq!(task.error_message, None);
+        assert_eq!(FangTaskState::New, task.state);
+        assert_eq!(Some(10), number);
+        assert_eq!(Some("PepeTask"), type_task);
     }
 
     #[test]
+    #[serial]
     fn fetch_task_fetches_the_oldest_task() {
         let task1 = PepeTask { number: 10 };
         let task2 = PepeTask { number: 11 };
@@ -611,25 +610,28 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task1 = Queue::insert_query(conn, &task1, Utc::now()).unwrap();
-            let _task2 = Queue::insert_query(conn, &task2, Utc::now()).unwrap();
+        let task1 = queue.insert_task(&task1).unwrap();
+        let _task2 = queue.insert_task(&task2).unwrap();
 
-            let found_task = Queue::fetch_task_query(conn, COMMON_TYPE.to_string()).unwrap();
-            assert_eq!(found_task.id, task1.id);
-            Ok(())
-        });
+        let found_task = queue.fetch_and_touch_task(COMMON_TYPE.to_string())
+            .unwrap()
+            .unwrap();
+
+        assert_eq!(found_task.id, task1.id);
     }
 
     #[test]
+    #[serial]
     fn update_task_state_test() {
         let task = PepeTask { number: 10 };
 
         let pool = Queue::connection_pool(5);
 
         let queue = Queue::builder().connection_pool(pool).build();
+
+        queue.remove_all_tasks().unwrap();
 
         let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
 
@@ -653,6 +655,7 @@ mod queue_tests {
     }
 
     #[test]
+    #[serial]
     fn fail_task_updates_state_field_and_sets_error_message() {
         let task = PepeTask { number: 10 };
 
@@ -660,30 +663,27 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task = Queue::insert_query(conn, &task, Utc::now()).unwrap();
+        let task = queue.insert_task(&task).unwrap();
 
-            let error = "Failed".to_string();
+        let error = "Failed";
 
-            let found_task = Queue::fail_task_query(conn, &task, &error).unwrap();
+        let found_task = queue.fail_task(&task, error).unwrap();
 
-            let metadata = found_task.metadata.as_object().unwrap();
-            let number = metadata["number"].as_u64();
-            let type_task = metadata["type"].as_str();
+        let metadata = found_task.metadata.as_object().unwrap();
+        let number = metadata["number"].as_u64();
+        let type_task = metadata["type"].as_str();
 
-            assert_eq!(found_task.id, task.id);
-            assert_eq!(found_task.state, FangTaskState::Failed);
-            assert_eq!(Some(10), number);
-            assert_eq!(Some("PepeTask"), type_task);
-            assert_eq!(found_task.error_message.unwrap(), error);
-
-            Ok(())
-        });
+        assert_eq!(found_task.id, task.id);
+        assert_eq!(found_task.state, FangTaskState::Failed);
+        assert_eq!(Some(10), number);
+        assert_eq!(Some("PepeTask"), type_task);
+        assert_eq!(found_task.error_message.unwrap(), error);
     }
 
     #[test]
+    #[serial]
     fn fetch_and_touch_updates_state() {
         let task = PepeTask { number: 10 };
 
@@ -691,46 +691,40 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task = Queue::insert_query(conn, &task, Utc::now()).unwrap();
+        let task = queue.insert_task(&task).unwrap();
 
-            let found_task = Queue::fetch_and_touch_query(conn, COMMON_TYPE.to_string())
-                .unwrap()
-                .unwrap();
+        let found_task = queue.fetch_and_touch_task(COMMON_TYPE.to_string())
+            .unwrap()
+            .unwrap();
 
-            let metadata = found_task.metadata.as_object().unwrap();
-            let number = metadata["number"].as_u64();
-            let type_task = metadata["type"].as_str();
+        let metadata = found_task.metadata.as_object().unwrap();
+        let number = metadata["number"].as_u64();
+        let type_task = metadata["type"].as_str();
 
-            assert_eq!(found_task.id, task.id);
-            assert_eq!(found_task.state, FangTaskState::InProgress);
-            assert_eq!(Some(10), number);
-            assert_eq!(Some("PepeTask"), type_task);
-
-            Ok(())
-        });
-    }
+        assert_eq!(found_task.id, task.id);
+        assert_eq!(found_task.state, FangTaskState::InProgress);
+        assert_eq!(Some(10), number);
+        assert_eq!(Some("PepeTask"), type_task);
+   }
 
     #[test]
+    #[serial]
     fn fetch_and_touch_returns_none() {
         let pool = Queue::connection_pool(5);
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let found_task = Queue::fetch_and_touch_query(conn, COMMON_TYPE.to_string()).unwrap();
+        let found_task = queue.fetch_and_touch_task(COMMON_TYPE.to_string()).unwrap();
 
-            assert_eq!(None, found_task);
-
-            Ok(())
-        });
+        assert_eq!(None, found_task);
     }
 
     #[test]
+    #[serial]
     fn insert_task_uniq_test() {
         let task = PepeTask { number: 10 };
 
@@ -738,78 +732,63 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task1 = Queue::insert_query(conn, &task, Utc::now()).unwrap();
-            let task2 = Queue::insert_query(conn, &task, Utc::now()).unwrap();
-
-            assert_eq!(task2.id, task1.id);
-            Ok(())
-        });
+        let task1 = queue.insert_task(&task).unwrap();
+        let task2 = queue.insert_task(&task).unwrap();
+        assert_eq!(task2.id, task1.id);
     }
 
     #[test]
+    #[serial]
     fn schedule_task_test() {
         let pool = Queue::connection_pool(5);
         let queue = Queue::builder().connection_pool(pool).build();
+        queue.remove_all_tasks().unwrap();
+        let datetime = (Utc::now() + Duration::seconds(7)).round_subsecs(0);
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        let task = &ScheduledPepeTask {
+            number: 10,
+            datetime: datetime.to_string(),
+        };
+        let task = queue.schedule_task(task).unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let datetime = (Utc::now() + Duration::seconds(7)).round_subsecs(0);
+        let metadata = task.metadata.as_object().unwrap();
+        let number = metadata["number"].as_u64();
+        let type_task = metadata["type"].as_str();
 
-            let task = &ScheduledPepeTask {
-                number: 10,
-                datetime: datetime.to_string(),
-            };
-
-            let task = Queue::schedule_task_query(conn, task).unwrap();
-
-            let metadata = task.metadata.as_object().unwrap();
-            let number = metadata["number"].as_u64();
-            let type_task = metadata["type"].as_str();
-
-            assert_eq!(Some(10), number);
-            assert_eq!(Some("ScheduledPepeTask"), type_task);
-            assert_eq!(task.scheduled_at, datetime);
-
-            Ok(())
-        });
+        assert_eq!(Some(10), number);
+        assert_eq!(Some("ScheduledPepeTask"), type_task);
+        assert_eq!(task.scheduled_at, datetime);
     }
 
     #[test]
+    #[serial]
     fn remove_all_scheduled_tasks_test() {
         let pool = Queue::connection_pool(5);
         let queue = Queue::builder().connection_pool(pool).build();
+        queue.remove_all_tasks().unwrap();
+        let datetime = (Utc::now() + Duration::seconds(7)).round_subsecs(0);
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        let task1 = &ScheduledPepeTask {
+            number: 10,
+            datetime: datetime.to_string(),
+        };
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let datetime = (Utc::now() + Duration::seconds(7)).round_subsecs(0);
+        let task2 = &ScheduledPepeTask {
+            number: 11,
+            datetime: datetime.to_string(),
+        };
 
-            let task1 = &ScheduledPepeTask {
-                number: 10,
-                datetime: datetime.to_string(),
-            };
+        queue.schedule_task(task1).unwrap();
+        queue.schedule_task(task2).unwrap();
 
-            let task2 = &ScheduledPepeTask {
-                number: 11,
-                datetime: datetime.to_string(),
-            };
-
-            Queue::schedule_task_query(conn, task1).unwrap();
-            Queue::schedule_task_query(conn, task2).unwrap();
-
-            let number = Queue::remove_all_scheduled_tasks_query(conn).unwrap();
-
-            assert_eq!(2, number);
-
-            Ok(())
-        });
+        let number = queue.remove_all_scheduled_tasks().unwrap();
+        assert_eq!(2, number);
     }
 
     #[test]
+    #[serial]
     fn remove_all_tasks_test() {
         let task1 = PepeTask { number: 10 };
         let task2 = PepeTask { number: 11 };
@@ -818,23 +797,20 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task1 = Queue::insert_query(conn, &task1, Utc::now()).unwrap();
-            let task2 = Queue::insert_query(conn, &task2, Utc::now()).unwrap();
+        let task1 = queue.insert_task(&task1).unwrap();
+        let task2 = queue.insert_task(&task2).unwrap();
+        
+        let result = queue.remove_all_tasks().unwrap();
 
-            let result = Queue::remove_all_tasks_query(conn).unwrap();
-
-            assert_eq!(2, result);
-            assert_eq!(None, Queue::find_task_by_id_query(conn, task1.id));
-            assert_eq!(None, Queue::find_task_by_id_query(conn, task2.id));
-
-            Ok(())
-        });
+        assert_eq!(2, result);
+        assert_eq!(None, queue.find_task_by_id(task1.id));
+        assert_eq!(None, queue.find_task_by_id(task2.id));
     }
 
     #[test]
+    #[serial]
     fn remove_task() {
         let task1 = PepeTask { number: 10 };
         let task2 = PepeTask { number: 11 };
@@ -843,25 +819,22 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task1 = Queue::insert_query(conn, &task1, Utc::now()).unwrap();
-            let task2 = Queue::insert_query(conn, &task2, Utc::now()).unwrap();
+        let task1 = queue.insert_task(&task1).unwrap();
+        let task2 = queue.insert_task(&task2).unwrap();
+        
+        assert!(queue.find_task_by_id(task1.id).is_some());
+        assert!(queue.find_task_by_id(task2.id).is_some());
 
-            assert!(Queue::find_task_by_id_query(conn, task1.id).is_some());
-            assert!(Queue::find_task_by_id_query(conn, task2.id).is_some());
+        queue.remove_task(task1.id).unwrap();
 
-            Queue::remove_task_query(conn, task1.id).unwrap();
-
-            assert!(Queue::find_task_by_id_query(conn, task1.id).is_none());
-            assert!(Queue::find_task_by_id_query(conn, task2.id).is_some());
-
-            Ok(())
-        });
+        assert!(queue.find_task_by_id(task1.id).is_none());
+        assert!(queue.find_task_by_id(task2.id).is_some());
     }
 
     #[test]
+    #[serial]
     fn remove_task_of_type() {
         let task1 = PepeTask { number: 10 };
         let task2 = AyratTask { number: 10 };
@@ -870,25 +843,22 @@ mod queue_tests {
 
         let queue = Queue::builder().connection_pool(pool).build();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        queue.remove_all_tasks().unwrap();
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task1 = Queue::insert_query(conn, &task1, Utc::now()).unwrap();
-            let task2 = Queue::insert_query(conn, &task2, Utc::now()).unwrap();
+        let task1 = queue.insert_task(&task1).unwrap();
+        let task2 = queue.insert_task(&task2).unwrap();
 
-            assert!(Queue::find_task_by_id_query(conn, task1.id).is_some());
-            assert!(Queue::find_task_by_id_query(conn, task2.id).is_some());
+        assert!(queue.find_task_by_id(task1.id).is_some());
+        assert!(queue.find_task_by_id(task2.id).is_some());
 
-            Queue::remove_tasks_of_type_query(conn, "weirdo").unwrap();
+        queue.remove_tasks_of_type("weirdo").unwrap();
 
-            assert!(Queue::find_task_by_id_query(conn, task1.id).is_some());
-            assert!(Queue::find_task_by_id_query(conn, task2.id).is_none());
-
-            Ok(())
-        });
+        assert!(queue.find_task_by_id(task1.id).is_some());
+        assert!(queue.find_task_by_id(task2.id).is_none());
     }
 
     #[test]
+    #[serial]
     fn remove_task_by_metadata() {
         let m_task1 = PepeTask { number: 10 };
         let m_task2 = PepeTask { number: 11 };
@@ -897,25 +867,19 @@ mod queue_tests {
         let pool = Queue::connection_pool(5);
 
         let queue = Queue::builder().connection_pool(pool).build();
+        queue.remove_all_tasks().unwrap();
+        let task1 = queue.insert_task(&m_task1).unwrap();
+        let task2 = queue.insert_task(&m_task2).unwrap();
+        let task3 = queue.insert_task(&m_task3).unwrap();
 
-        let mut queue_pooled_connection = queue.connection_pool.get().unwrap();
+        assert!(queue.find_task_by_id(task1.id).is_some());
+        assert!(queue.find_task_by_id(task2.id).is_some());
+        assert!(queue.find_task_by_id(task3.id).is_some());
 
-        queue_pooled_connection.test_transaction::<(), Error, _>(|conn| {
-            let task1 = Queue::insert_query(conn, &m_task1, Utc::now()).unwrap();
-            let task2 = Queue::insert_query(conn, &m_task2, Utc::now()).unwrap();
-            let task3 = Queue::insert_query(conn, &m_task3, Utc::now()).unwrap();
+        queue.remove_task_by_metadata(&m_task1).unwrap();
 
-            assert!(Queue::find_task_by_id_query(conn, task1.id).is_some());
-            assert!(Queue::find_task_by_id_query(conn, task2.id).is_some());
-            assert!(Queue::find_task_by_id_query(conn, task3.id).is_some());
-
-            Queue::remove_task_by_metadata_query(conn, &m_task1).unwrap();
-
-            assert!(Queue::find_task_by_id_query(conn, task1.id).is_none());
-            assert!(Queue::find_task_by_id_query(conn, task2.id).is_some());
-            assert!(Queue::find_task_by_id_query(conn, task3.id).is_some());
-
-            Ok(())
-        });
+        assert!(queue.find_task_by_id(task1.id).is_none());
+        assert!(queue.find_task_by_id(task2.id).is_some());
+        assert!(queue.find_task_by_id(task3.id).is_some());
     }
 }
